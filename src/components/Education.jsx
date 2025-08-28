cae446ac8950a3ecbed72732a6264c1e7240aabb
FPabloA <pabloayala@protonmail.com>
2025-08-28 09:36:58 -0500

    finished implementation of education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,16 +7,19 @@ function Education () {
     const [dateofStudy, setDateOfStudy] = useState('');
     const [submitted, setSubmit] = useState(false);
 
-    const handleSchoolChange = () => {
-        //TODO
+    const handleSchoolChange = (e) => {
+        const newSchool = e.target.value;
+        setSchoolName(newSchool);
     }
 
-    const handleTitleChange = () => {
-        //TODO
+    const handleTitleChange = (e) => {
+        const newTitle = e.target.value;
+        setTitleOfStudy(newTitle);
     }
 
-    const handleDateChange = () => {
-        //TODO
+    const handleDateChange = (e) => {
+        const newDate = e.target.value;
+        setDateOfStudy(newDate);
     }
 
     const handleSubmit = () => {
@@ -25,10 +28,43 @@ function Education () {
     }
 
     if(submitted) {
-        //TODO
+        return (
+            <>
+                <div className='education-wrapper'>
+                    <div className='education-header'>Education</div>
+                    <div className='education-input-wrapper'>
+                        <div className='education-label'>School Name: {schoolName}</div>
+
+                        <div className='education-label'>Title of Study: {titleOfStudy}</div>
+                        
+                        <div className='education-label'>Date of Study: {dateofStudy}</div>
+
+                    </div>
+                    <button onClick={handleSubmit} id='education-edit'>Edit</button>
+                </div>
+            </>
+        )
     }
     else {
-        //TODO
+        return (
+            <>
+                <div className='education-wrapper'>
+                    <div className='education-header'>Education</div>
+                    <div className='education-input-wrapper'>
+                        <label className='education-label' for='education-school'>School Name:</label>
+                        <input className='education-input' id='education-school' type='text' placeholder='Enter school name' value={schoolName} onChange={handleSchoolChange}></input>
+
+                        <label className='education-label' for='education-title'>Title of Study:</label>
+                        <input className='education-input' id='education-title' type='text' placeholder='Enter title of study' value={titleOfStudy} onChange={handleTitleChange}></input>
+
+                        <label className='education-label' for='education-date'>Date of Study:</label>
+                        <input className='education-input' id='education-date' type='date' placeholder='Enter date of study' value={dateofStudy} onChange={handleDateChange}></input>
+                    </div>
+                    <button onClick={handleSubmit} id='education-submit'>Submit</button>
+                </div>
+            </>
+        )
+        
     }
 }
 
